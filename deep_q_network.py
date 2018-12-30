c51ef74799dc60cb6c22b8b9e8f3a67888b0a8f3
robert1ridley <robert_n_ridley@instance-1.us-central1-c.c.modern-replica-226115.internal>
2018-12-30 04:30:31 +0000

    checkpoint

--- deep_q_network.py
+++ deep_q_network.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
-# import os
-# os.environ["SDL_VIDEODRIVER"] = "dummy"
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 import tensorflow as tf
 import cv2
